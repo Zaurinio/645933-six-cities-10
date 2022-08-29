05cbd0b98e95165d95003b3dcc388e7ef57d047f
Заур <zaurinio@inbox.ru>
2022-08-30 00:31:42 +0300

    Правки перед проверкой (замечания наставника)

--- project/src/store/comments-data/comments-data.ts
+++ project/src/store/comments-data/comments-data.ts
@@ -6,6 +6,7 @@ import { fetchCommentAction, postCommentAction } from '../api-actions';
 const initialState: CommentsData = {
   comments: [],
   areCommentsDwonloaded: false,
+  isCommentFormSubmited: false,
 };
 
 export const commentsData = createSlice({
@@ -21,8 +22,12 @@ export const commentsData = createSlice({
         state.comments = action.payload;
         state.areCommentsDwonloaded = true;
       })
+      .addCase(postCommentAction.pending, (state) => {
+        state.isCommentFormSubmited = true;
+      })
       .addCase(postCommentAction.fulfilled, (state, action) => {
         state.comments = action.payload;
+        state.isCommentFormSubmited = false;
       });
   }
 });
--- project/src/store/comments-data/selectors.ts
+++ project/src/store/comments-data/selectors.ts
@@ -4,3 +4,4 @@ import { Comments } from '../../types/places';
 
 export const getComments = (state: State): Comments => state[NameSpace.Comments].comments;
 export const getLoadedCommentsStatus = (state: State): boolean => state[NameSpace.Comments].areCommentsDwonloaded;
+export const getCommentFormSubmitStatus = (state: State): boolean => state[NameSpace.Comments].isCommentFormSubmited;
